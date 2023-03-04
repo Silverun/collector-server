c185b448a9dbaa4be2e9f515b49929a69d3527a6
silverun <imonutiy@gmail.com>
2023-03-04 23:41:44 +0300

    cookie conf

--- config/cookies.js
+++ config/cookies.js
@@ -3,7 +3,7 @@ const cookiesOpts = {
   maxAge: 24 * 60 * 60 * 1000,
   secure: true,
   sameSite: "None",
-  domain: ".colltest.herokuapp.com",
+  domain: ".cltr.herokuapp.com",
 };
 
 module.exports = cookiesOpts;
