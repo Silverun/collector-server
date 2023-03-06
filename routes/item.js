b22c970ae907cc0c470548de76ded15f1fc13674
silverun <imonutiy@gmail.com>
2023-03-06 11:48:18 +0300

    123

--- routes/item.js
+++ routes/item.js
@@ -8,16 +8,18 @@ const verifyStatus = require("../middleware/verifyStatus");
 router.get("/getall", itemController.getAllItems);
 router.get("/gettags", itemController.getCloudTags);
 router.get("/getsearchitems", itemController.getSearchItems);
+
 router.get("/:col_id", itemController.getCollectionItems);
 router.get("/:item_id/getcomments", itemController.getItemComments);
 router.get("/:item_id/getlikes", itemController.getItemLikes);
-router.get("/:item_id/removelike/:user_id", itemController.removeItemLike);
 
+router.post("/:id", itemController.getItem);
+
+router.get("/:item_id/removelike/:user_id", itemController.removeItemLike);
 router.post("/new", itemController.createItem);
 router.post("/addcoment", itemController.addItemComment);
 router.post("/addlike", itemController.addItemLike);
 router.post("/:item_id/update", itemController.updateItem);
 router.post("/:item_id/delete", itemController.deleteItem);
-router.post("/:id", itemController.getItem);
 
 module.exports = router;
