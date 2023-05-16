b65790ba6f7520efea6be36854a84c0342cee1df
silverun <imonutiy@gmail.com>
2023-05-16 20:06:41 +0300

    render move

--- config/cookies.js
+++ config/cookies.js
@@ -3,7 +3,7 @@ const cookiesOpts = {
   maxAge: 24 * 60 * 60 * 1000,
   secure: true,
   sameSite: "None",
-  domain: ".cltr.herokuapp.com",
+  domain: ".cltrserver.onrender.com",
 };
 
 module.exports = cookiesOpts;
